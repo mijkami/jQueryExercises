a9f5d63f89d691312c43e3e3a49d72a58c68e78a
mijkami <dimitri.gaillot@gmail.com>
2019-05-14 10:25:16 +0200

    fix main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+// Pour l'instant le seul script présent est la fonction ready(), 
+//    qui attend que le navigateur finisse de construire l'arborescence DOM
+//    avant de lancer le script compris entre les crochets {} encore vides.
+//
+//
+// Nous avons des images avec une classe 'product-photo' mais elles 
+// n'apparaissent pas sur le site, car leur propriété display est réglée sur :
+// 
+//    .product-photo {
+//      display: none;
+//    }
+//
 // Sélectionnez via jQuery la classe correspondante et appliquez une méthode
 // permettant de l'afficher !
 //
@@ -5,6 +17,7 @@
 // 		.product-photo
 //		.show() (ou .toggle())
 // 		$();
+
 
 $(document).ready(() => {
 	
