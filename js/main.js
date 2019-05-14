868909aebc8d7fce33e54264772d5b6e03993f09
mijkami <dimitri.gaillot@gmail.com>
2019-05-14 10:15:10 +0200

    fix main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-// Pour l'instant le seul script présent est la fonction ready(), 
-//    qui attend que le navigateur finisse de construire l'arborescence DOM
-//    avant de lancer le script compris entre les crochets {} encore vides.
-//
-//
-// Nous avons des images avec une classe 'product-photo' mais elles 
-// n'apparaissent pas sur le site, car leur propriété display est réglée sur :
-// 
-//    .product-photo {
-//      display: none;
-//    }
-//
 // Sélectionnez via jQuery la classe correspondante et appliquez une méthode
 // permettant de l'afficher !
 //
@@ -23,8 +11,9 @@ $(document).ready(() => {
 });
 
 
+
 // En manipulant uniquement le DOM (pas de class/ID) :
-// 	Dans la navbar affichée en haut, ajoutez à chaque 2nd élément
+// 	Dans la navbar affichée en (haut logo, menu, login) , ajoutez à chaque 2nd élément
 //	de la liste l'élément : "<span> - 2nd!</span>"
 
 
@@ -35,7 +24,7 @@ $(document).ready(() => {
 
 
 // Toujours en manipulant uniquement le DOM : 
-//	changez la couleur du menu de la navbar (logo, menu, login) en bleu 
+//	changez la couleur (du background) du menu de la navbar (logo, menu, login) en bleu 
 
 $(document).ready(() => {
 	
@@ -43,7 +32,7 @@ $(document).ready(() => {
 
 
 // Toujours en manipulant uniquement le DOM : 
-// changez la couleur du menu "dropdown" en dessous de la navbar en rouge
+// changez la couleur (du background) du menu "dropdown" (shoes & contact + les listes) en dessous de la navbar en rouge
 
 $(document).ready(() => {
 	
@@ -51,10 +40,10 @@ $(document).ready(() => {
 
 
 
-// Maintenant, sélectionnez l'id nav-dropdown et cachez-moi cette navbar !
+// Maintenant, sélectionnez l'id nav-dropdown (shoes & contact + les listes) et cachez-moi cette navbar !
 
 $(document).ready(() => {
-	
+  
 });
 
 
@@ -62,9 +51,3 @@ $(document).ready(() => {
 
 // Vous êtes prêts pour les futures instructions, 
 // 	restez à l'écoute (il se passe sûrement quelque chose au tableau)
-
-
-
-
-
-
